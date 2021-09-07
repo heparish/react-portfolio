617f843c6732c6f7e12a152e99442c32ecfbcd10
Haley Parish <heparish@ucdavis.edu>
2021-09-06 22:32:16 -0700

    Updates

---
+++ precache-manifest.a67edf537136a9abd7856eada90e2e75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fa16516377ee0f574a8",
+    "url": "/react-portfolio/static/css/main.4466b505.chunk.css"
+  },
+  {
+    "revision": "4fa16516377ee0f574a8",
+    "url": "/react-portfolio/static/js/main.4fa16516.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "e8d48dcec411e2d9d69edcd18cd56723",
+    "url": "/react-portfolio/index.html"
+  }
+];
